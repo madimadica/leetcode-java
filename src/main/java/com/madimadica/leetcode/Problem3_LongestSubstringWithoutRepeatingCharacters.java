4ccc611daaa7dca93b09efd5d9c54ae1571e07b0
Madi <145222821+madimadica@users.noreply.github.com>
2025-01-05 12:37:25 -0600

    Solve Problem 3

---
+++ src/main/java/com/madimadica/leetcode/Problem3_LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,48 @@
+package com.madimadica.leetcode;
+
+import java.util.BitSet;
+
+public class Problem3_LongestSubstringWithoutRepeatingCharacters {
+    /**
+     * Given a string s, find the length of the longest
+     * substring without repeating characters.
+     * <br>
+     * <p>
+     *     Constraints: Consists of english letters, digits, symbols, and spaces
+     * </p>
+     * @param s String to search
+     * @return length of the longest unique substring
+     */
+    public int lengthOfLongestSubstring(String s) {
+        final int len = s.length();
+
+
+        int bestWindow = 0;
+        int windowStart = 0;
+        BitSet windowCodepoints = new BitSet();
+
+        // Sliding window from windowStart to i;
+        for (int i = 0; i < len; ++i) {
+            int currentCodepoint = s.codePointAt(i);
+            boolean seen = windowCodepoints.get(currentCodepoint);
+            if (seen) {
+                bestWindow = Math.max(bestWindow, i - windowStart);
+                // Move the start of the sliding window forward until the duplicate
+                for (int j = windowStart; j < i; ++j) {
+                    int headCodepoint = s.codePointAt(j);
+                    if (headCodepoint == currentCodepoint) {
+                        windowStart = j + 1;
+                        break;
+                    }
+                    // Un-see the others
+                    windowCodepoints.set(headCodepoint, false);
+                }
+            } else {
+                windowCodepoints.set(currentCodepoint, true);
+            }
+        }
+        // Goalpost problem:
+        bestWindow = Math.max(bestWindow, len - windowStart);
+        return bestWindow;
+    }
+}
