f73cf503ed65fefb2c5bf38ce6f35a0e7438790e
Madi <145222821+madimadica@users.noreply.github.com>
2025-01-06 00:02:32 -0600

    Update ListNode API: Static factories and toString
    For easier debugging and manual testing of problematic inputs.

--- src/main/java/com/madimadica/leetcode/api/ListNode.java
+++ src/main/java/com/madimadica/leetcode/api/ListNode.java
@@ -1,5 +1,9 @@
 package com.madimadica.leetcode.api;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
 // Definition for singly-linked list.
 public class ListNode {
     public int val;
@@ -17,11 +21,43 @@ public class ListNode {
         this.next = next;
     }
 
+    public static ListNode of(Integer... values) {
+        return ListNode.of(List.of(values));
+    }
+
+    public static ListNode of(List<Integer> values) {
+        if (values.isEmpty()) {
+            return null;
+        }
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+        for (int val : values) {
+            tail.next = new ListNode(val);
+            tail = tail.next;
+        }
+        return dummy.next;
+    }
+
     @Override
     public String toString() {
-        return "ListNode{" +
-                "val=" + val +
-                ", next=" + next +
-                '}';
+        StringJoiner sj = new StringJoiner(" -> ");
+
+        ListNode current = this;
+        List<ListNode> seen = new ArrayList<>();
+        boolean cycle = false;
+        while (current != null) {
+            if (seen.contains(current)) {
+                cycle = true;
+                break;
+            }
+            seen.add(current);
+            sj.add(String.valueOf(current.val));
+            current = current.next;
+        }
+
+        if (cycle) {
+            sj.add("[CYCLE [%d]]".formatted(current.val));
+        }
+        return sj.toString();
     }
 }
